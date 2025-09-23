be21ec136d20e21b83814a8ce3e374cbd9148a74
Dakotus of Borg <dakota.cahill@protonmail.com>
2025-09-23 16:20:07 -0400

    Initial commit refreshing the entire project

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -21,16 +21,16 @@
 #include "MainWindow.h"
 #include "Game.h"
 
-Game::Game(MainWindow& wnd)
+Game::Game( MainWindow& wnd )
 	:
-	wnd(wnd),
-	gfx(wnd)
+	wnd( wnd ),
+	gfx( wnd )
 {
 }
 
 void Game::Go()
 {
-	gfx.BeginFrame();
+	gfx.BeginFrame();	
 	UpdateModel();
 	ComposeFrame();
 	gfx.EndFrame();
@@ -38,130 +38,8 @@ void Game::Go()
 
 void Game::UpdateModel()
 {
-	if (wnd.kbd.KeyIsPressed(VK_RIGHT))
-	{
-		x_mobile = x_mobile + 1;
-	}
-
-	if (wnd.kbd.KeyIsPressed(VK_LEFT))
-	{
-		x_mobile = x_mobile - 1;
-	}
-
-	if (wnd.kbd.KeyIsPressed(VK_DOWN))
-	{
-		y_mobile = y_mobile + 1;
-	}
-
-	if (wnd.kbd.KeyIsPressed(VK_UP))
-	{
-		y_mobile = y_mobile - 1;
-	}
-
-	x_mobile = ClampScreenX(x_mobile);
-	y_mobile = ClampScreenX(y_mobile);
-
-	colliding =
-		OverlapTest(x_fixed0, y_fixed0, x_mobile, y_mobile) ||
-		OverlapTest(x_fixed1, y_fixed1, x_mobile, y_mobile) ||
-		OverlapTest(x_fixed2, y_fixed2, x_mobile, y_mobile) ||
-		OverlapTest(x_fixed3, y_fixed3, x_mobile, y_mobile);
 }
 
 void Game::ComposeFrame()
 {
-	DrawBox(x_fixed0, y_fixed0, 0, 255, 0);
-	DrawBox(x_fixed1, y_fixed1, 0, 255, 0);
-	DrawBox(x_fixed2, y_fixed2, 0, 255, 0);
-	DrawBox(x_fixed3, y_fixed3, 0, 255, 0);
-
-	if (colliding)
-	{
-		DrawBox(x_mobile, y_mobile, 255, 0, 0);
-	}
-	else
-	{
-		DrawBox(x_mobile, y_mobile, 255, 255, 255);
-	}
 }
-
-void Game::DrawBox(int x, int y, int r, int g, int b)
-{
-	gfx.PutPixel(-5 + x, -5 + y, r, g, b);
-	gfx.PutPixel(-5 + x, -4 + y, r, g, b);
-	gfx.PutPixel(-5 + x, -3 + y, r, g, b);
-	gfx.PutPixel(-4 + x, -5 + y, r, g, b);
-	gfx.PutPixel(-3 + x, -5 + y, r, g, b);
-	gfx.PutPixel(-5 + x, 5 + y, r, g, b);
-	gfx.PutPixel(-5 + x, 4 + y, r, g, b);
-	gfx.PutPixel(-5 + x, 3 + y, r, g, b);
-	gfx.PutPixel(-4 + x, 5 + y, r, g, b);
-	gfx.PutPixel(-3 + x, 5 + y, r, g, b);
-	gfx.PutPixel(5 + x, -5 + y, r, g, b);
-	gfx.PutPixel(5 + x, -4 + y, r, g, b);
-	gfx.PutPixel(5 + x, -3 + y, r, g, b);
-	gfx.PutPixel(4 + x, -5 + y, r, g, b);
-	gfx.PutPixel(3 + x, -5 + y, r, g, b);
-	gfx.PutPixel(5 + x, 5 + y, r, g, b);
-	gfx.PutPixel(5 + x, 4 + y, r, g, b);
-	gfx.PutPixel(5 + x, 3 + y, r, g, b);
-	gfx.PutPixel(4 + x, 5 + y, r, g, b);
-	gfx.PutPixel(3 + x, 5 + y, r, g, b);
-}
-
-bool Game::OverlapTest(int box0x, int box0y, int box1x, int box1y)
-{
-	const int left_box0 = box0x - 5;
-	const int right_box0 = box0x + 5;
-	const int top_box0 = box0y - 5;
-	const int bottom_box0 = box0y + 5;
-
-	const int left_box1 = box1x - 5;
-	const int right_box1 = box1x + 5;
-	const int top_box1 = box1y - 5;
-	const int bottom_box1 = box1y + 5;
-
-	return
-		left_box0 <= right_box1 &&
-		right_box0 >= left_box1 &&
-		top_box0 <= bottom_box1 &&
-		bottom_box0 >= top_box1;
-}
-
-int Game::ClampScreenX(int x)
-{
-	const int left = x - 5;
-	const int right = x + 5;
-	if (left < 0)
-	{
-		return 5;
-	}
-	else if (right >= gfx.ScreenWidth)
-	{
-		return gfx.ScreenWidth - 6;
-	}
-	else
-	{
-		return x;
-	}
-}
-
-int Game::ClampScreenY(int y)
-{
-	const int top = y - 5;
-	const int bottom = y + 5;
-	if (top < 0)
-	{
-		return 5;
-	}
-	else if (bottom >= gfx.ScreenHeight)
-	{
-		return gfx.ScreenHeight - 6;
-	}
-	else
-	{
-		return y;
-	}
-}
-
-
